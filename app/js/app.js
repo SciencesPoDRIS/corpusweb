dc3c2fc768c62f1c1db6fc257610520dbc16bcd3
Anne Lhote <anne.lhote@gmail.com>
2015-05-04 22:48:57 +0200

    Add query search on list entities

--- app/js/app.js
+++ app/js/app.js
@@ -2,8 +2,10 @@
     'use strict';
 
     var app = angular.module('webcorpus', [
+        // 'elasticjs.service',
         'ui.bootstrap',
-        'ngRoute'
+        'ngRoute',
+        'elasticsearch'
     ]);
 
     app.controller('CorpusSnippetCtrl',
@@ -38,6 +40,118 @@
         }
     ]);
 
+
+    // We define an Angular controller that returns the server health
+    // Inputs: $scope and the 'es' service
+
+    // Create the es service from the esFactory
+    app.service('es', function(esFactory) {
+        return esFactory({
+            host: 'localhost:9200'
+        });
+    });
+
+    // We define an Angular controller that returns query results,
+    // Inputs: $scope and the 'es' service
+
+    app.controller('QueryController',
+        function($scope, es) {
+            $scope.search = function() {
+                    if (!$scope.queryTerm == '') {
+                        es.search({
+                            index: 'anne-test',
+                            size: 10,
+                            body: {
+                                "query": {
+                                    "query_string": {
+                                        "query": "*" + $scope.queryTerm.toLowerCase() + "*",
+                                        "fields": ["name", "start pages"]
+                                    }
+                                },
+                            }
+                        }).then(function(response) {
+                            $scope.hits = response.hits.hits;
+                        });
+                    } else {
+                        es.search({
+                            index: 'anne-test',
+                            size: 10,
+                            body: {
+                                "query": {
+                                    "match_all": {}
+                                },
+                            }
+                        }).then(function(response) {
+                            $scope.hits = response.hits.hits;
+                        });
+                    }
+                }
+                // Load all results
+            $scope.search();
+        }
+    );
+
+    app.controller('SearchCtrl', ['$scope', 'ejsResource',
+        function($scope, ejsResource) {
+            var ejs = ejsResource('http://localhost:9200');
+            var index = 'anne-test';
+
+            var highlightPost = ejs.Highlight(["text"])
+                .fragmentSize(150, "text")
+                .numberOfFragments(1, "text")
+                .preTags("<b>", "text")
+                .postTags("</b>", "text");
+
+            var hashtagFacet = ejs.TermsFacet('Hashtag')
+                .field('hashtag.text')
+                .size(10);
+
+            var statusRequest = ejs.Request()
+                .indices(index)
+                .types('webentity')
+                .highlight(highlightPost)
+                .facet(hashtagFacet);
+
+            var activeFilters = {};
+            var results = {};
+
+            $scope.resultsArr = [];
+
+            $scope.search = function() {
+                console.log('search');
+                $scope.resultsArr = [];
+                if (!$scope.queryTerm == '') {
+                    results = statusRequest
+                        .query(applyFilters(ejs.MatchQuery('_all', $scope.queryTerm)))
+                        // .fields(['name', 'prefixes', 'start pages'])
+                        .doSearch();
+                    $scope.resultsArr.push(results);
+                    console.log($scope.resultsArr);
+                } else {
+                    results = {};
+                    $scope.resultsArr = [];
+                    activeFilters = {};
+                }
+            }
+
+            var applyFilters = function(query) {
+                var filter = null;
+                var filters = Object.keys(activeFilters).map(function(k) {
+                    return activeFilters[k];
+                });
+                // if more than one filter, use AND operator
+                if (filters.length > 1) {
+                    filter = ejs.AndFilter(filters);
+                } else if (filters.length === 1) {
+                    filter = filters[0];
+                }
+
+                return filter ? ejs.FilteredQuery(query, filter) : query;
+            };
+        }
+    ]);
+
+    /*
     app.controller('WebEntitiesListCtrl', ['$scope', '$http',
         function($scope, $http) {
             $http.get('../data/corpora.json').success(function(data) {
@@ -64,6 +178,7 @@
             });
         }
     ]);
+    */
 
     app.controller('CartoCtrl',
         function() {
