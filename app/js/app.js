93acf125f70324f67fcf6aac12d3e7176b7c4118
Anne Lhote <anne.lhote@gmail.com>
2015-05-26 17:10:13 +0200

    Reorganize HTML structure, use angular-material-design, add webentities thumbnail, add pagination

--- app/js/app.js
+++ app/js/app.js
@@ -4,7 +4,6 @@
     var app = angular.module('corpusweb', [
         'ui.bootstrap',
         'ngRoute',
-        'elasticsearch',
         'ngMaterial'
     ]);
 
@@ -25,160 +24,90 @@
         }
     ]);
 
-    app.controller('CorpusSnippetCtrl',
+    app.controller('CorpusSnippetCtrl', ['$scope', '$http',
         function($scope, $http) {
-            $http.get('../data/corpora.json').success(function(data) {
-                $scope.corpus = data.corpora[0];
-            });
-        }
-    );
-
-    app.controller('CorpusCtrl', ['$scope', '$routeParams', '$http',
-        function($scope, $routeParams, $http) {
             $http.get('../data/corpora.json').success(function(data) {
                 $scope.corpus = data.corpora[0];
             });
         }
     ]);
 
-
-    // We define an Angular controller that returns the server health
-    // Inputs: $scope and the 'es' service
-
-    // Create the es service from the esFactory
-    app.service('es', function(esFactory) {
-        return esFactory({
-            host: 'localhost:9200'
-        });
-    });
-
-    // We define an Angular controller that returns query results,
-    // Inputs: $scope and the 'es' service
-
-    app.controller('QueryController',
-        function($scope, $http, es) {
-            // Initialise search input
-            $scope.selectedItem = 0;
-            $scope.searchText = '';
-
-            // Initialise slider default value
-            $scope.slider = 1;
-
-            // Load facets
-            $scope.facets = [];
+    app.controller('CorpusCtrl', ['$scope', '$http',
+        function($scope, $http) {
             $http.get('../data/corpora.json').success(function(data) {
-                $scope.facets = data.corpora[0].facets;
+                $scope.corpus = data.corpora[0];
             });
+        }
+    ]);
 
-            // Launch search
-            $scope.search = function() {
-                    if (!$scope.queryTerm == '') {
-                        es.search({
-                            index: 'anne-test',
-                            size: 10,
-                            body: {
-                                "query": {
-                                    "query_string": {
-                                        "query": "*" + $scope.queryTerm.toLowerCase() + "*",
-                                        "fields": ["name", "start pages"]
-                                    }
-                                },
-                            }
-                        }).then(function(response) {
-                            $scope.tiles = response.hits.hits;
-                        });
-                    } else {
-                        es.search({
-                            index: 'anne-test',
-                            size: 10,
-                            body: {
-                                "query": {
-                                    "match_all": {}
-                                },
-                            }
-                        }).then(function(response) {
-                            $scope.tiles = response.hits.hits;
-                        });
-                    }
-                }
-                // Initialize search as full search
-            $scope.search();
+    app.controller('QueryController', ['$scope', '$http',
+        function($scope, $http) {
+            $scope.queryTerm = '';
+            $scope.totalItems = 0;
+            $scope.currentPage = 1;
+            $scope.numPerPage = 10;
+            var ids = new Array();
+            var begin = 0;
+            var end = 0;
 
-            $scope.formatFacet = function(values) {
-                return values.sort().map(function(value) {
-                    return {
-                        value: value.toLowerCase(),
-                        display: value.charAt(0).toUpperCase() + value.slice(1).toLowerCase()
-                    }
-                });
-            }
-
+            // Load the graph
             sigma.parsers.gexf(
-                '../../data/arctic.gexf', {
-                    container: 'carto'
+                '../../data/COP21.gexf', {
+                    container: 'carto',
+                    settings: {
+                        defaultEdgeColor: '#d3d3d3',
+                        edgeColor: '#d3d3d3'
+                    }
                 },
                 function(s) {
+                    s.graph.nodes().forEach(function(n) {
+                        n.color = '#707070';
+                    });
                     s.refresh();
+                    $scope.sig = s;
                 }
             );
-        }
-    );
-
-    app.controller('SearchCtrl', ['$scope', 'ejsResource',
-        function($scope, ejsResource) {
-            var ejs = ejsResource('http://localhost:9200');
-            var index = 'anne-test';
-
-            var highlightPost = ejs.Highlight(["text"])
-                .fragmentSize(150, "text")
-                .numberOfFragments(1, "text")
-                .preTags("<b>", "text")
-                .postTags("</b>", "text");
-
-            var hashtagFacet = ejs.TermsFacet('Hashtag')
-                .field('hashtag.text')
-                .size(10);
-
-            var statusRequest = ejs.Request()
-                .indices(index)
-                .types('webentity')
-                .highlight(highlightPost)
-                .facet(hashtagFacet);
-
-            var activeFilters = {};
-            var results = {};
-
-            $scope.resultsArr = [];
 
             $scope.search = function() {
-                $scope.resultsArr = [];
-                if (!$scope.queryTerm == '') {
-                    results = statusRequest
-                        .query(applyFilters(ejs.MatchQuery('_all', $scope.queryTerm)))
-                        // .fields(['name', 'prefixes', 'start pages'])
-                        .doSearch();
-                    $scope.resultsArr.push(results);
-                } else {
-                    results = {};
-                    $scope.resultsArr = [];
-                    activeFilters = {};
-                }
+                begin = ($scope.currentPage - 1) * 10;
+                end = begin + $scope.numPerPage;
+                $http.get('../data/COP21.csv').success(function(data) {
+                    $scope.allResults = $.csv.toObjects(data).slice(1);
+                    $scope.results = $scope.allResults.filter(function(item) {
+                        return item.NAME.includes($scope.queryTerm);
+                    });
+                    $scope.totalItems = $scope.results.length;
+                    $scope.results = $scope.results.slice(begin, end);
+                    $scope.results.forEach(function(result) {
+                        result.url = result['START PAGES'].split(' ').concat(result['PREFIXES'].split(' ')).filter(function(item) {
+                            return item;
+                        })[0];
+                    });
+                    // Aggregate all the ids of the selected nodes
+                    ids = $scope.results.map(function(item) {
+                        return item.ID;
+                    });
+                    if (!$scope.queryTerm) {
+                        // Reset all nodes' color to the default one
+                        $scope.sig.graph.nodes().forEach(function(n) {
+                            n.color = '#707070';
+                        });
+                    } else {
+                        // Reset all nodes' color to the light grey
+                        $scope.sig.graph.nodes().forEach(function(node) {
+                            node.color = '#d3d3d3';
+                        });
+                        // Color selected nodes into red
+                        $scope.sig.graph.nodes().forEach(function(node) {
+                            if (ids.indexOf(node.id) != -1) node.color = '#e6142d';
+                        });
+                    }
+                    $scope.sig.refresh();
+                });
             }
 
-            var applyFilters = function(query) {
-                var filter = null;
-                var filters = Object.keys(activeFilters).map(function(k) {
-                    return activeFilters[k];
-                });
-                // if more than one filter, use AND operator
-                if (filters.length > 1) {
-                    filter = ejs.AndFilter(filters);
-                } else if (filters.length === 1) {
-                    filter = filters[0];
-                }
-
-                return filter ? ejs.FilteredQuery(query, filter) : query;
-            };
+            // Initiate the search results
+            $scope.search();
         }
     ]);
 })();
